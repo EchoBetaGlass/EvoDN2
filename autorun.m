3ff31d8127609bf9fd20d14cfecf98146e11e26c
Bhupinder <bhupinder.gmail2.71@gmail.com>
2018-07-20 11:21:22 +0530

    Added selection of pareto network feature

--- autorun.m
+++ autorun.m
@@ -8,14 +8,16 @@ warning off
 	Problem_name = 'ZDT1_100';
 	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	EvDtrain.in_index = [1:30];
-	EvDtrain.out_index = [31];
+	EvDtrain.out_index = [32];
 	EvDtrain.generations = 50; % 10 max generations for evolution
-	EvDtrain.Pop_str{1}{1} = [1:30];           %maximum number of nodes
-	EvDtrain.Pop_str{1}{2} = [30 15];
+	EvDtrain.Pop_str{1}{1} = [1:15];           %maximum number of nodes
+	EvDtrain.Pop_str{1}{2} = [15 15];
+    EvDtrain.Pop_str{2}{1} = [15:30];           %maximum number of nodes
+	EvDtrain.Pop_str{2}{2} = [16 15];
 	EvDtrain.Prey_popsize = 80;      %500 Initial popsize
-	EvDtrain.no_Prey_preferred = 50; %500 Desired popsize
+	EvDtrain.no_Prey_preferred = 100; %500 Desired popsize
 	EvDtrain.no_new_Prey = 50;       %500 new prey introduced every KillInterval
-	EvDtrain.Predator_popsize = 15;  %Number of Predators
+	EvDtrain.Predator_popsize = 40;  %Number of Predators
 	EvDtrain.no_x = 20;              %lattice size (no of rows)
 	EvDtrain.no_y = 20;              %lattice size (no of cols)
 	EvDtrain.ploton = 1;            %set 0 for no plots or 1 for plots at every generation
--- f_EvoDN_net.m
+++ f_EvoDN_net.m
@@ -1,28 +1,33 @@
 function f_EvoDN_net(parameters)
     setno = 1;
+    select = 1;
 	in = parameters.dataset(setno).in;
 	out = parameters.dataset(setno).out;
-    no_layer = length(parameters.dataset.pareto.P);
-    w = (parameters.dataset.pareto.P);
-    select = parameters.dataset.pareto.select;
-	select = 1;
+    Pop_str = parameters.Pop_str;
+    net = (parameters.dataset.pareto.P(select));
+    endnet = (parameters.dataset.pareto.P(select).endnet);
+    num_subnets = length(Pop_str);
+    in_end = [];
+    %select = parameters.dataset.pareto.select;
+	
     outmax = max(out);
 	outmin = min(out);
 	no_points = length(out);
-    net = {};
-    for layer = 1:no_layer
-        net{layer}(:,:) = squeeze(w{layer}.w(select,:,:));  
+    for subnet = 1:num_subnets
+        in = parameters.dataset(setno).in;
+        in = in(:,Pop_str{subnet}{1});
+        no_layer = length(Pop_str{subnet}{2});
+        snet = net.subnet{subnet};
+        for layer = 1:no_layer - 1  %-1?
+            in = in*snet{layer}(2:end,:);
+            bias = snet{layer}(1,:);
+            in = in+bias;
+            in = 1./(1+exp(-in));
+        end
+        in_end = [in_end in];
     end
-	for layer = 1:no_layer  %-1?
-		in = in*net{layer}(2:end,:);
-		bias = net{layer}(1,:);
-		in = in+bias;
-		in = 1./(1+exp(-in));
-    end
-	%% LLSQ to solve in*Beta = out
-	Beta = in\out;
     hold on
- 	modelout = in*Beta;
+ 	modelout = in_end*endnet;
     plot(out,modelout,'kd','LineWidth', 2)
     pol = polyfit(out,modelout,1);
     ylim=get(gca,'ylim');
@@ -30,7 +35,7 @@ function f_EvoDN_net(parameters)
     text(xlim(1)+0.1*(xlim(2)-xlim(1)),ylim(2),['\fontsize{12} \color{red} Slope =' num2str(pol(1))])
     pol = pol(1)*out + pol(2);
     plot(out,pol, '.-b', 'LineWidth', 2)
-   % plot([1:no_points],out,'--o',[1:no_points],modelout,'-or')
+    %plot([1:no_points],out,'--o',[1:no_points],modelout,'-or')
     hold off
 
 end
